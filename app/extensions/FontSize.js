92d765cbeccedf6f5c2a2cb1ff09d277d6c4398c
onewritescode <lalmohansingh83@gmail.com>
2026-01-12 12:56:01 +0530

    adding editor

---
+++ app/extensions/FontSize.js
@@ -0,0 +1,49 @@
+import { Mark, mergeAttributes } from '@tiptap/core'
+
+export const FontSize = Mark.create({
+  name: 'fontSize',
+
+  addOptions() {
+    return {
+      HTMLAttributes: {},
+    }
+  },
+
+  addAttributes() {
+    return {
+      fontSize: {
+        default: null,
+        parseHTML: element => element.style.fontSize || null,
+        renderHTML: attributes => {
+          if (!attributes.fontSize) return {}
+          return {
+            style: `font-size: ${attributes.fontSize}`,
+          }
+        },
+      },
+    }
+  },
+
+  parseHTML() {
+    return [
+      {
+        tag: 'span[style]',
+        style: 'font-size',
+      },
+    ]
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ['span', mergeAttributes(this.options.HTMLAttributes, HTMLAttributes), 0]
+  },
+
+  addCommands() {
+    return {
+      setFontSize:
+        fontSize =>
+        ({ commands }) => {
+          return commands.setMark('fontSize', { fontSize })
+        },
+    }
+  },
+})
