23e94aab193bc2dfe88b14a837b00bd8b625cead
onewritescode <lalmohansingh83@gmail.com>
2026-01-13 15:32:06 +0530

    yourday page

--- app/(root)/day/page.tsx
+++ app/(root)/day/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Editor from "@/app/components/Editor";
+import YourDay from "@/app/components/YourDay";
 
 type tasksDoneType = {
   taskName: string;
@@ -9,89 +9,7 @@ type tasksDoneType = {
 };
 
 function page() {
-  const [task, setTask] = useState<string>();
-  const [tasksDone, setTasksDone] = useState<tasksDoneType[]>([]);
-  useEffect(() => {
-    setTasksDone((prev) => [
-      { taskName: "task", isDone: true },
-      { taskName: "task", isDone: true },
-      { taskName: "task", isDone: true },
-      { taskName: "task", isDone: true },
-      { taskName: "task", isDone: true },
-      { taskName: "task", isDone: true },
-      { taskName: "task", isDone: true },
-      { taskName: "task", isDone: true },
-      { taskName: "task", isDone: true },
-      { taskName: "task", isDone: true },
-      { taskName: "task", isDone: true },
-      { taskName: "task", isDone: true },
-      { taskName: "task", isDone: true },
-      { taskName: "task", isDone: true },
-      { taskName: "task", isDone: true },
-      { taskName: "task", isDone: true },
-      { taskName: "task", isDone: true },
-      { taskName: "task", isDone: true },
-      ...prev,
-    ]);
-  }, []);
-
-  const addTask = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (!task?.trim()) return;
-
-    setTasksDone((prev) => [{ taskName: task, isDone: true }, ...prev]);
-    setTask("");
-  };
-
-  return (
-    <div className="bg-black text-white min-h-screen flex items-center justify-center ovreflow-hidden flex-col p-4">
-      <div className="w-xl sm:w-4xl xl:w-5xl lg:w-6xl">
-        <div className="border border-gray-700 rounded-xl p-4 w-full flex items-center justify-between">
-          <p className="capitalize">things you did today</p>
-          <form className="flex items-center gap-2">
-            <input
-              type="text"
-              className="border-b border-b-gray-600 py-1 outline-none"
-              value={task}
-              onChange={(e) => setTask(e.target.value)}
-            />
-            <button
-              className="bg-blue-600 rounded-sm p-1 uppercase cursor-pointer"
-              onClick={addTask}
-            >
-              add
-            </button>
-          </form>
-        </div>
-
-        <div className="flex gap-2 flex-wrap pt-4 w-full overflow-hidden capitalize">
-          {tasksDone.map((singleTask, idx) => (
-            <div
-              key={idx}
-              className="px-2 py-1 border- boder-gray-700 bg-white/10 rounded-md"
-            >
-              {singleTask.taskName}
-              {singleTask.isDone}
-            </div>
-          ))}
-        </div>
-
-        {/* <div className="border border-gray-700 rounded-xl overflow-hidden w-full my-4">
-          <label htmlFor="whatClickedToday" className="p-2 capitalize">
-            what clicked today:
-          </label>
-          <textarea
-            name="whatClickedToday"
-            id="whatClickedToday"
-            className="p-2 py-4 w-full outline-none border-none"
-          />
-        </div> */}
-        <div className="bg-[var(--background-color)] mt-10 text-gray-300 text-sm funnel-sans">
-          <Editor />
-        </div>
-      </div>
-    </div>
-  );
+  return <YourDay />;
 }
 
 export default page;
---
+++ app/components/YourDay.tsx
@@ -0,0 +1,258 @@
+"use client";
+
+import { Color, TextStyle } from "@tiptap/extension-text-style";
+import { useEditor } from "@tiptap/react";
+import { EditorContent } from "@tiptap/react";
+import Heading from "@tiptap/extension-heading";
+import StarterKit from "@tiptap/starter-kit";
+import Highlight from "@tiptap/extension-highlight";
+import { FontSize } from "@/app/extensions/FontSize";
+import Image from "@tiptap/extension-image";
+import { Redo, Undo } from "lucide-react";
+import { useEffect, useState } from "react";
+
+type Editor = {};
+
+type tasksDoneType = {
+  taskName: string;
+  isDone: boolean;
+};
+
+export default function Editor() {
+  // saving data to database
+  const SaveToDb = async () => {
+    if (!editor) return;
+    const content = editor.getJSON().toString();
+
+    const res = await fetch("/api/journal", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        journalContent: content,
+        tasks: tasksDone,
+      }),
+    });
+    if (!res.ok) {
+      console.log("here you go with response from backend", res);
+    }
+  };
+
+  // fetching journaldata form backned
+  useEffect(() => {
+    const fetchedData = async () => {
+      const data = await fetch("api/journal").then((res) => res.json());
+      console.log(data);
+    };
+  }, []);
+
+  // logics and script for task component
+  const [task, setTask] = useState<string>();
+  const [tasksDone, setTasksDone] = useState<tasksDoneType[]>([]);
+
+  const addTask = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!task?.trim()) return;
+
+    setTasksDone((prev) => [{ taskName: task, isDone: true }, ...prev]);
+    setTask("");
+  };
+
+  // logics and configration for editor
+  const defaultContent = `
+        <strong>You Can Write There About Your Day</strong>
+        <br />
+        <br />
+    `;
+
+  const editor: Editor | any = useEditor({
+    extensions: [
+      StarterKit.configure({
+        heading: false,
+      }),
+      Image.configure({
+        inline: false,
+        allowBase64: true,
+      }),
+      TextStyle,
+      Highlight,
+      FontSize,
+      Heading.configure({
+        levels: [1, 2, 3, 4, 5, 6],
+      }),
+    ],
+    content: defaultContent,
+
+    editorProps: {
+      handlePaste(view, event) {
+        const items = Array.from(event.clipboardData?.items || []);
+        const imageItem = items.find((item) => item.type.includes("image"));
+
+        if (imageItem) {
+          const file: File | any = imageItem.getAsFile();
+          const reader = new FileReader();
+          reader.onload = () => {
+            const src = reader.result;
+            editor
+              .chain()
+              .focus()
+              .setImage({
+                src,
+                width: "300px",
+                height: "auto",
+              })
+              .run();
+          };
+          reader.readAsDataURL(file);
+          return true;
+        }
+        return false;
+      },
+    },
+    // setting this only to avoid errors form next.js server side rendering
+    immediatelyRender: false,
+  });
+
+  return (
+    <div className="w-full bg-black text-white min-h-screen flex items-center justify-center ovreflow-hidden flex-col p-4 overflow-hidden">
+      <div className=" sm:min-w-2xl md:min-w-3xl xl:min-w-6xl">
+        <div className="border border-gray-700 rounded-xl p-4 w-full flex items-center justify-between">
+          <p className="capitalize">things you did today</p>
+          <form className="flex items-center gap-2">
+            <input
+              type="text"
+              className="border-b border-b-gray-600 py-1 outline-none"
+              value={task}
+              onChange={(e) => setTask(e.target.value)}
+            />
+            <button
+              className="bg-blue-600 rounded-sm p-1 uppercase cursor-pointer"
+              onClick={addTask}
+            >
+              add
+            </button>
+          </form>
+        </div>
+
+        <div className="flex gap-2 flex-wrap pt-4 w-full overflow-hidden capitalize">
+          {tasksDone.map((singleTask, idx) => (
+            <div
+              key={idx}
+              className="px-2 py-1 border- boder-gray-700 bg-white/10 rounded-md"
+            >
+              {singleTask.taskName}
+              {singleTask.isDone}
+            </div>
+          ))}
+        </div>
+        {/* editor */}
+        <div className="bg-(--background-color) mt-10 text-gray-300 text-sm funnel-sans">
+          <div className="relative w-full">
+            <div className="absolute top-0 p-6 w-full flex items-center justify-center z-1000">
+              <div className="inline-flex items-center justify-center rounded-xl bg-[var(--light-background)] px-3 py-1 gap-1 mr-3">
+                {/* undo button */}
+                <button
+                  onClick={() => editor.chain().focus().undo().run()}
+                  className="h-6 w-6 hover:bg-[var(--background-color)] flex items-center justify-center rounded-full cursor-pointer"
+                >
+                  <Undo size={18} />
+                </button>
+
+                {/* redo button */}
+                <button
+                  onClick={() => editor.chain().focus().redo().run()}
+                  className="h-6 w-6 hover:bg-[var(--background-color)] flex items-center justify-center rounded-full cursor-pointer"
+                >
+                  <Redo size={18} />
+                </button>
+              </div>
+
+              <div className="inline-flex items-center justify-center rounded-xl bg-[var(--light-background)] px-3 py-1 gap-1">
+                {/* bold  */}
+                <button
+                  onClick={() => editor.chain().focus().toggleBold().run()}
+                  className="h-6 hover:bg-black px-2 font-bold rounded-md cursor-pointer"
+                >
+                  Bold
+                </button>
+
+                {/* italic  */}
+                <button
+                  onClick={() => editor.chain().focus().toggleItalic().run()}
+                  className="h-6 hover:bg-black px-2 italic rounded-md cursor-pointer"
+                >
+                  Italic
+                </button>
+
+                {/* strike */}
+                <button
+                  onClick={() => editor.chain().focus().toggleStrike().run()}
+                  className="h-6 hover:bg-black px-2 line-through rounded-md cursor-pointer"
+                >
+                  Strike
+                </button>
+
+                {/* horizontal line  */}
+                <button
+                  onClick={() =>
+                    editor.chain().focus().setHorizontalRule().run()
+                  }
+                  className="h-6 text-sm rounded-md font-medium px-2 cursor-pointer hover:bg-black"
+                >
+                  Horizontal rule
+                </button>
+
+                {/* highlight */}
+                <button
+                  onClick={() => editor.chain().focus().toggleHighlight().run()}
+                  className="h-6 text-sm rounded-md font-medium px-2 cursor-pointer hover:bg-black"
+                >
+                  Highlight
+                </button>
+
+                {/* select font size  */}
+                <select
+                  onChange={(e) =>
+                    editor.chain().focus().setFontSize(e.target.value).run()
+                  }
+                  className="outline-none px-2 py-1 rounded max-h-100 overflow-y-auto [&::-webkit-scrollbar]:w-1  [&::-webkit-scrollbar-track]:bg-neutral-700  [&::-webkit-scrollbar-thumb]:bg-neutral-500 cursor-pointer"
+                >
+                  {Array.from({ length: 100 }, (_, i) => (
+                    <option
+                      key={i}
+                      value={`${i}px`}
+                      className="h-6 w-6 text-sm bg-black/90"
+                    >
+                      {i}px
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
+            <div className="max-w-4x  w-full flex items-start justify-center px-10 pt-20">
+              <div
+                className="editorContent w-full py  rounded-xl shadow-lg prose prose-lg leading-relaxed "
+                onClick={() => editor?.commands.focus()}
+              >
+                <EditorContent
+                  editor={editor}
+                  className="outline-none min-h-[40vh] cursor-text"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full flex justify-end items-end mt-4">
+          <button
+            onClick={SaveToDb}
+            className="px-4 py-1 rounded-md font-bold capitalize bg-blue-600 cursor-pointer"
+          >
+            submit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
