34a0153451df6b5297094685897e1874b344caf7
onewritescode <lalmohansingh83@gmail.com>
2026-01-21 17:03:05 +0530

    logo thing

--- app/components/LoginPage.tsx
+++ app/components/LoginPage.tsx
@@ -8,8 +8,10 @@ function LoginPage() {
           className="pl-1 pr-10 py-1 rounded-3xl bg-white/10 cursor-pointer group border border-gray-700 flex items-center gap-4 font-bold"
           onClick={() => signIn("google")}
         >
-          <img src="/GoogleLogo.png" alt="logo" className="w-7 h-7" />
-    <span className="group-hover:scale-108 transition-all">Sign in with Google</span>
+          <img src="/images/GoogleLogo.png" alt="logo" className="w-8 h-8" />
+          <span className="group-hover:scale-108 transition-all">
+            Sign in with Google
+          </span>
         </button>
       </div>
     </div>
@@ -17,4 +19,3 @@ function LoginPage() {
 }
 
 export default LoginPage;
-
